dd9eb7992bb2b53c50d5a77e7d24d3f005d3db05
Austin Hunt <ahunt03087@gmail.com>
2024-05-10 17:09:56 -0400

    added getPlayerChoice and getComputerChoice Functions

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,38 @@
 console.log("Hello World")
+console.log(getPlayerChoice())
+
+function getComputerChoice()
+{
+    choice = Math.floor((Math.random()*3))
+    switch(choice)
+    {
+        case 0:
+            return "rock"
+            break;
+        case 1:
+            return "paper"
+            break;
+        case 2:
+            return "scissors"
+            break;
+        default:
+            throw new Error("Undefined choice")
+    }
+}
+
+function getPlayerChoice() 
+{
+    let keepGoing = true;
+    let userInput
+    while(keepGoing)
+    {
+        userInput = prompt("Please choose one of either rock, paper, or scissors")
+        console.log(userInput)
+        goodInput = userInput == "rock" || userInput == "paper" || userInput == "scissors"
+        if(goodInput)
+        {
+            keepGoing = false
+            return userInput
+        }
+    }
+}
